ecf2753091aa4a771403330a9d220465b64990c1
Patrick Thomas <patrickthomas101x@gmail.com>
2026-09-25 15:45:47 -0500

    Add c_list

---
+++ c_list/include/collections/export.h
@@ -0,0 +1,10 @@
+#ifndef EXPORT_H
+#define EXPORT_H
+
+#ifndef C_LIST_STATIC_DEFINE
+    #include <c_list/export_shared.h>
+#else
+    #include <c_list/export_static.h>
+#endif // #ifndef C_LIST_STATIC_DEFINE
+
+#endif // #ifndef EXPORT_H
---
+++ c_list/tests/c_list_test.cpp
@@ -0,0 +1,10 @@
+// Google Test Includes
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+// Local Includes
+#include <collections/c_list.h>
+
+namespace c_list_testing {
+
+} // namespace c_list_testing
