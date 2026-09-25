3ec5fb16687ca6932acf9902590d36d9c5c95b8f
Patrick Thomas <patrickthomas101x@gmail.com>
2026-09-25 15:59:39 -0500

    Add collections prefix

--- c_vector/include/collections/c_vector.h
+++ c_vector/include/collections/c_vector.h
@@ -1,5 +1,5 @@
-#ifndef C_VECTOR_H
-#define C_VECTOR_H
+#ifndef C_COLLECTIONS_VECTOR_H
+#define C_COLLECTIONS_VECTOR_H
 
 #ifdef __cplusplus
 extern "C" {
@@ -9,4 +9,4 @@ extern "C" {
 }
 #endif // #ifdef __cplusplus
 
-#endif // #ifndef C_VECTOR_H
+#endif // #ifndef C_COLLECTIONS_VECTOR_H
