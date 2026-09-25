408a6aed409a7f0bbd0cab1eb53e6b48e9b7aea8
Patrick Thomas <patrickthomas101x@gmail.com>
2026-09-25 15:45:38 -0500

    Add c_array

---
+++ c_array/include/collections/export.h
@@ -0,0 +1,10 @@
+#ifndef EXPORT_H
+#define EXPORT_H
+
+#ifndef C_ARRAY_STATIC_DEFINE
+    #include <c_array/export_shared.h>
+#else
+    #include <c_array/export_static.h>
+#endif // #ifndef C_ARRAY_STATIC_DEFINE
+
+#endif // #ifndef EXPORT_H
---
+++ c_array/tests/c_array_test.cpp
@@ -0,0 +1,10 @@
+// Google Test Includes
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+// Local Includes
+#include <collections/c_array.h>
+
+namespace c_array_testing {
+
+} // namespace c_array_testing
