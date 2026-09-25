2fbe8d480d2cba89a989d9cb0947ad789c4a3808
Patrick Thomas <patrickthomas101x@gmail.com>
2026-09-25 08:27:56 -0500

    Add c_array_tests.cpp

---
+++ c_array/tests/c_array_tests.cpp
@@ -0,0 +1,15 @@
+// ISO C Includes
+
+// ISO C++ Includes
+#include <array>
+
+// Google Test Includes
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+// Local Includes
+#include <collections/array.h>
+
+namespace collections::c_array_testing {
+    
+} // namespace collections::c_array_testing
