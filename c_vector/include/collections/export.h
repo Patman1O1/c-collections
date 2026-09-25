266689327d6d5c5919babc1c09b9280f4c601826
Patrick Thomas <patrickthomas101x@gmail.com>
2026-09-25 15:45:52 -0500

    Add c_vector

---
+++ c_vector/include/collections/export.h
@@ -0,0 +1,10 @@
+#ifndef EXPORT_H
+#define EXPORT_H
+
+#ifndef C_VECTOR_STATIC_DEFINE
+    #include <c_vector/export_shared.h>
+#else
+    #include <c_vector/export_static.h>
+#endif // #ifndef C_VECTOR_STATIC_DEFINE
+
+#endif // #ifndef EXPORT_H
---
+++ c_vector/tests/c_vector_test.cpp
@@ -0,0 +1,10 @@
+// Google Test Includes
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+// Local Includes
+#include <collections/c_vector.h>
+
+namespace c_vector_testing {
+
+} // namespace c_vector_testing
